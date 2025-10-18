22b28816c5a65937039f8d7eb81acd0d0182f166
Havennnn <latrellmarable00@gmail.com>
2025-10-18 12:36:22 +0800

    Route changes

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,15 @@ Route::middleware('auth')->group(function () {
         return view('dashboard');
     })->name('dashboard');
 
-    Route::resource('tasks', TaskController::class);
+    // Route::resource('tasks', TaskController::class);
+
+    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+    Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+    Route::get('/tasks/{id}', [TaskController::class, 'show'])->name('tasks.show');
+    Route::get('/tasks/{id}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::put('/tasks/{id}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::delete('/tasks/{id}', [TaskController::class, 'delete'])->name('tasks.delete');
 });
 
 require __DIR__.'/auth.php';
